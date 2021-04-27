eec70835207d16032f66866be299f9e11884820e
SHINOZAKI-RUOSUKE <gibson335m@gmail.com>
2021-04-27 11:16:28 +0000

    [+Gem_devise]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,9 +25,9 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    book= Book.find(params[:id])  # データ（レコード）を1件取得
-    book.destroy  # データ（レコード）を削除
-    redirect_to books_path  # 投稿一覧画面へリダイレクト
+    book= Book.find(params[:id]) 
+    book.destroy 
+    redirect_to books_path
   end
 
 
