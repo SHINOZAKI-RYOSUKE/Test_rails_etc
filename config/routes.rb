3257bf6209a2c26109de277186bc1b7b04870a91
SHINOZAKI-RUOSUKE <gibson335m@gmail.com>
2021-04-28 11:19:05 +0000

    [+nest]

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "post_images#index"
   get 'homes/top'
-  resources :post_images, only:[:new, :create, :index, :show, :destroy]
+  resources :post_images, only:[:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only:[:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
